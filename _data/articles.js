e58ecd93a7eb8597c0eec4844ff36f3cc0cbc3cd
seongwonkc <seongwonkc@gmail.com>
2025-08-15 02:14:13 +0900

    Delete _data directory

--- _data/articles.js
+++
@@ -1,7 +0,0 @@
-// _data/articles.js
-module.exports = async function() {
-  // This is a placeholder. In a more advanced setup, you could fetch
-  // articles from a CMS here. For now, Eleventy's collection system
-  // will handle finding all the article files automatically.
-  return [];
-};
