0a386bc2f5c94966bb693a300e5771aed748e1cf
seongwonkc <seongwonkc@gmail.com>
2025-08-15 02:13:16 +0900

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,14 @@
 // .eleventy.js
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
   // This command tells Eleventy to copy your 'assets' folder to the final build.
   eleventyConfig.addPassthroughCopy("assets");
+
+  // Custom filter to format dates for display (e.g., "Aug 15, 2025")
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("LLL dd, yyyy");
+  });
 
   return {
     // These are the directories where Eleventy will look for files.
